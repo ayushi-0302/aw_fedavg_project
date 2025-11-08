ee08ea8ae4b63e89dc0878f74ee1c484feab5b5d
ayushi-41 <22070510@ycce.in>
2025-11-08 15:24:01 +0530

    Initial commit

---
+++ create_structure.bat
@@ -0,0 +1,15 @@
+mkdir src\utils
+mkdir src\baselines
+mkdir src\experiments
+
+type nul > src\utils\heterogeneity.py
+type nul > src\utils\staleness_sim.py
+type nul > src\utils\metrics.py
+type nul > src\utils\config.py
+
+type nul > src\baselines\fedavg.py
+type nul > src\baselines\fedprox.py
+type nul > src\baselines\scaffold.py
+
+type nul > src\experiments\exp_config_base.json
+type nul > src\experiments\run_experiments.py
